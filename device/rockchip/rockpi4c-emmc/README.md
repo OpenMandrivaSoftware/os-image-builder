ad16d44d63861df395e119bf44ed1710498b135a
Bernhard Rosenkränzer <bero@lindev.ch>
2021-01-03 17:24:35 +0100

    Add rockpi4c-emmc target

---
+++ device/rockchip/rockpi4c-emmc/README.md
@@ -0,0 +1,3 @@
+# Installing on Rock Pi 4C
+Write the generated rockpi4c.img to an SD card with dd
+Insert the SD card and boot the board
---
+++ device/rockchip/rockpi4c-emmc/boot.cmd
@@ -0,0 +1,7 @@
+# dev 1 = SD card
+# dev 2 = EMMC
+mmc dev 1
+fatload mmc 1:1 ${kernel_addr_r} Image
+fatload mmc 1:1 ${fdt_addr_r} rockchip/rk3399-rock-pi-4c.dtb
+setenv bootargs init=/sbin/init console=tty1 console=ttyS2,1500000 no_console_suspend earlycon=uart,mmio32,0xff1a0000 swiotlb=1 coherent_pool=1m earlyprintk rw rootwait root=/dev/mmcblk2p2
+booti ${kernel_addr_r} - ${fdt_addr_r}
